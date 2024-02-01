7ad6876fc23313a6c81a1895a4ae9a441cbf43ba
boaltamirano <brayanaltamiranoperez@gmail.com>
2024-01-31 23:43:27 -0500

    feat: inizialization of go mod

---
+++ DesignPatterns/Estructural/Example/main.go
@@ -0,0 +1,10 @@
+// Implementation of design parttern adapter
+
+package main
+
+import "github.com/boaltamirano/go-practice/DesignPatterns/Estructural/Example/bicicleta"
+
+func main() {
+	b := bicicleta.Bicicleta{}
+	b.Avanzar()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/boaltamirano/go-practice
+
+go 1.21.6
