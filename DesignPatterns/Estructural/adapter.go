737b99a79be7138d27d027b0c50f827b24451205
boaltamirano <brayanaltamiranoperez@gmail.com>
2024-01-31 23:33:01 -0500

    feat: adapter pattern

---
+++ DesignPatterns/Estructural/adapter.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+type Payment interface {
+	Pay()
+}
+
+type CashPayment struct{}
+
+func (CashPayment) Pay() {
+	fmt.Println("Payment using Cash")
+}
+
+func ProcessPayment(p Payment) {
+	p.Pay()
+}
+
+type BankPayment struct{}
+
+func (BankPayment) Pay(bankAccount int) {
+	fmt.Printf("Paying using BankAccount %d\n", bankAccount)
+}
+
+type BankPaymentAdapter struct {
+	BankPayment *BankPayment
+	bankAccount int
+}
+
+func (bpa *BankPaymentAdapter) Pay() {
+	bpa.BankPayment.Pay(bpa.bankAccount)
+}
+
+func main() {
+	cash := &CashPayment{}
+	ProcessPayment(cash)
+
+	bpa := &BankPaymentAdapter{
+		bankAccount: 5,
+		BankPayment: &BankPayment{},
+	}
+
+	ProcessPayment(bpa)
+}
